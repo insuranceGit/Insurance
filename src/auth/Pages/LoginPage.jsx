c1f397a4ddbf8422278ab19e280076c24287086a
Juan David Torres <juantor98@gmail.com>
2022-10-26 23:10:18 -0500

    Taildwind + Primer commit de front-end
    Mejora en diseño de login, implementación esqueleto de table en opción de historial y formulario de solicitud de inscripción

--- src/auth/Pages/LoginPage.jsx
+++ src/auth/Pages/LoginPage.jsx
@@ -1,59 +1,66 @@
-import { Link as RouterLink } from "react-router-dom"
-
-import { Grid, Typography, TextField, Button, Link } from "@mui/material"
-import { Google } from "@mui/icons-material"
+// import { Link as RouterLink } from "react-router-dom"
+// import { Grid, Typography, TextField, Button, Link } from "@mui/material"
+// import { Google } from "@mui/icons-material"
 import { AuthLayout } from "../Layout/AuthLayout"
 
-
 export const LoginPage = () => {
-  return (
-    <AuthLayout title='Login'>
-      <form>
-          <Grid item xs={12} sx={{mt:2}}>
-            <TextField
-              label="Correo"
-              type="email"
-              //placeholder ="Ingrese correo"
-              fullWidth
-            ></TextField>              
-          </Grid>
-          <Grid item xs={12} sx={{mt:2}}>
-            <TextField
-              label="Contraseña"
-              type="password"
-              //placeholder ="Ingrese contraseña"
-              fullWidth
-            ></TextField>             
-          </Grid>
+    return (
+        <AuthLayout>
+            <section className="flex justify-center">
+                <div className="container px-6 py-12 h-full">
+                    <div className="flex justify-center items-center h-full g-6 text-gray-800">
+                        <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0 flex justify-center items-center">
+                            <img
+                                src="../../../public/ImgLogin.svg"
+                                alt="Login image"
+                            />
+                        </div>
+                        <div className="md:w-8/12 lg:w-6/12 lg:ml-20">
+                            <div className="text-center">
+                                <img
+                                    className="mx-auto w-48"
+                                    src="../../../public/Logo_AIR-fullcolor.png"
+                                    alt="logo"
+                                />
+                                <h4 className="text-xl font-semibold mt-1 mb-12 pb-1">
+                                    Somos AIR Seguros
+                                </h4>
+                            </div>
+                            <form className="">
+                                {/* <!-- Email input --> */}
+                                <div className="mb-6">
+                                    <input
+                                        type="text"
+                                        className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-800 bg-white bg-clip-padding border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none shadow-lg shadow-cyan-500/50"
+                                        placeholder="Usuario"
+                                    />
+                                </div>
 
-          <Grid container spacing={2} sx={{mb:2, mt:1}}>
-            <Grid item xs={12} sm={6}>
-            <Link component={ RouterLink } style={{ textDecoration: 'none' }} to='/'>
-              <Button variant='contained' fullWidth>
-                Login
-              </Button>
-            </Link>
-              
+                                {/* <!-- Password input --> */}
+                                <div className="mb-6">
+                                    <input
+                                        type="password"
+                                        className="form-control block w-full px-4 py-2 text-sm font-normal text-gray-800 bg-white bg-clip-padding border border-solid border-gray-300 rounded-lg transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none shadow-lg shadow-cyan-500/50"
+                                        placeholder="Password"
+                                    />
+                                </div>
 
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Button variant='contained' fullWidth>
-                <Google/>
-                <Typography sx={{ml:1}}>Google</Typography>
-              </Button>
-            </Grid>
-
-            <Grid container direction='row' justifyContent='end'>
-                <Link component={ RouterLink } color='inherit' to='/auth/register'>
-                  Crear cuenta
-                </Link>
-            </Grid>
-
-          </Grid>
-        </form>
-
-    </AuthLayout>
-        
-      
-  )
-}
+                                {/* <!-- Submit button --> */}
+                                    <div className="flex justify-center items-center">
+                                        <button
+                                            type="button"
+                                            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-lg hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                                            data-mdb-ripple="true"
+                                            data-mdb-ripple-color="light"
+                                        >
+                                            Sign in
+                                        </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </AuthLayout>
+    );
+};
--- src/insurances/pages/FormPage.jsx
+++ src/insurances/pages/FormPage.jsx
@@ -5,22 +5,58 @@ import { InsertDriveFile } from "@mui/icons-material"
 export const FormPage = () => {
   return (
     <InsuranceLayout>
-      <Grid
-      container
-      spacing={ 0 }
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: 'calc(100vh - 110px)', backgroundColor: 'primary.sec', borderRadius: 3 }}
-    >
-      <Grid item xs={ 12 }>
-        <InsertDriveFile sx={{fontSize:80, color:'black'}}/>          
-      </Grid>
-      <Grid item xs={ 12 }>
-        <Typography color='black' variant="h5">Vista de formulario no encontrada</Typography>          
-      </Grid>
-
-    </Grid>
+      <form className='border border-gray-300 p-6 rounded-lg'>
+          <h1 className='text-4xl text-blue-700'>Solicitud de afiliación</h1>
+          <hr/>
+          <br/>
+          <div className='grid gap-6 mb-6 md:grid-cols-4'>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Número de identificación</label>
+                  <input type='number' id='document' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Identificación' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Primer Nombre</label>
+                  <input type='text' id='first_name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Ingresa tu primer nombre' />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Segundo Nombre</label>
+                  <input type='text' id='second_name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Ingresa tu segundo nombre' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Primer Apellido</label>
+                  <input type='text' id='first_last_name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Ingresa tu primer apellido' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Segundo Apellido</label>
+                  <input type='text' id='second_last_name' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Ingresa tu segundo apellido' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Ciudad de residencia</label>
+                  <input type='text' id='city' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Ciudad' required/>
+              </div>  
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Dirección de residencia</label>
+                  <input type='text' id='address' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='Dirección completa sin símbolos' required/>
+              </div>  
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Celular</label>
+                  <input type='tel' id='cellphone' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='313-100-0000' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>Correo electrónico</label>
+                  <input type='email' id='email' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='@email.com' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>EPS</label>
+                  <input type='text' id='eps' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='' required />
+              </div>
+              <div>
+                  <label className='block mb-2 text-sm font-medium text-gray-900'>IPS</label>
+                  <input type='text' id='IPS' className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5' placeholder='' required />
+              </div>
+          </div>
+          <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Registrar</button>
+      </form>
     </InsuranceLayout>
   )
 }
