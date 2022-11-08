33e0dc80a40dea7e4ec2f58858920a3ae264e9ab
Jairo-Combita <jairoalbertocombita@gmail.com>
2022-11-08 12:45:03 -0500

    load

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage, RegisterPage } from "../pages";
+import { LoginPage, RegisterPage } from "src\auth\pages";
 
 
 export const AuthRoutes = () => {
